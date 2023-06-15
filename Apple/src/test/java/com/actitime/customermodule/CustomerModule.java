826ca0a665f7edc25cac51e915f2aea4d8c292d6
ASUS <ASUS@LAPTOP-5KCODA6R>
2023-06-15 15:34:29 +0530

    gg

---
+++ Apple/src/test/java/com/actitime/customermodule/CustomerModule.java
@@ -0,0 +1,21 @@
+package com.actitime.customermodule;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class CustomerModule {
+	static {
+		System.setProperty("webdriver.chrome.driver", "./src/main/resources/driver/chromedriver.exe");
+	}
+@Test
+public void createCustomer() {
+	WebDriver driver=new ChromeDriver();
+	driver.get("https://demo.actitime.com/login.do");
+	Reporter.log("Create Customer",true);
+	Reporter.log("Modify Customer",true);
+	Reporter.log("Delete Customer",true);
+	driver.close();
+	}
+}
---
+++ Apple/src/test/java/com/actitime/customermodule/ProjectModule.java
@@ -0,0 +1,12 @@
+package com.actitime.customermodule;
+
+import org.testng.annotations.Test;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class ProjectModule {
+@Test
+public void createProject() {
+	Reporter.log("createProject",true);
+	}
+}
